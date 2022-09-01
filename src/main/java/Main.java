9e531a7e8e5424603c3b46b285541e793ebe5da8
dzimk <dzimk87@yandex.ru>
2022-09-01 21:08:29 +0300

    0.0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
+        String  str;
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+        System.out.println(str);
     }
 }
